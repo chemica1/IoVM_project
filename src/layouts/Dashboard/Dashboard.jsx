6c66ed7c0d0d0903991a21b96425031404a47ebc
seungkyu2 <zx6658@naver.com>
2019-02-06 23:00:58 +0900

    fix: :hammer: tag 정리및 에러 해결

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -19,22 +19,22 @@ import dashboardStyle from "assets/jss/material-dashboard-react/layouts/dashboar
 import image from "assets/img/sidebar-2.jpg";
 import logo from "assets/img/reactlogo.png";
 
-import FirebaseAuth from '../../components/Login/firebaseUI';
-import StyledFirebaseAuth from '../../components/Login/StyledFirebaseUI';
+import FirebaseAuth from "../../components/Login/firebaseUI";
+import StyledFirebaseAuth from "../../components/Login/StyledFirebaseUI";
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/auth";
 
-import styles from '../../components/Login/app.css'; // This uses CSS modules.
-import '../../components/Login/firebaseui-styling.global.css'; // Import globally.
-import {DB_config} from "config/fbconfig.js"
-
+import styles from "../../components/Login/app.css"; // This uses CSS modules.
+import "../../components/Login/firebaseui-styling.global.css"; // Import globally.
+import { DB_config } from "config/fbconfig.js";
 
 const firebaseApp = firebase.initializeApp(DB_config);
 const switchRoutes = (
   <Switch>
     {dashboardRoutes.map((prop, key) => {
-      if (prop.redirect)   //이게 처음 들왔을때 화면이다.
+      if (prop.redirect)
+        //이게 처음 들왔을때 화면이다.
         return <Redirect from={prop.path} to={prop.to} key={key} />;
       return <Route path={prop.path} component={prop.component} key={key} />;
     })}
@@ -46,44 +46,45 @@ class App extends React.Component {
     super(props);
     this.state = {
       mobileOpen: false,
-      isSignedIn: undefined,
+      isSignedIn: undefined
     };
-    
+
     this.resizeFunction = this.resizeFunction.bind(this);
   }
 
   uiConfig = {
-    signInFlow: 'popup',
+    signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
-      signInSuccessWithAuthResult: () => false,
-    },
+      signInSuccessWithAuthResult: () => false
+    }
   };
-
 
   handleDrawerToggle = () => {
     this.setState({ mobileOpen: !this.state.mobileOpen });
   };
   getRoute() {
     return this.props.location.pathname !== "/maps";
-  };
+  }
   resizeFunction() {
     if (window.innerWidth >= 960) {
       this.setState({ mobileOpen: false });
     }
-  };
+  }
   componentDidMount() {
     if (navigator.platform.indexOf("Win") > -1) {
       const ps = new PerfectScrollbar(this.refs.mainPanel);
     }
-    this.unregisterAuthObserver = firebaseApp.auth().onAuthStateChanged((user) => {
-      this.setState({isSignedIn: !!user});
-    });
+    this.unregisterAuthObserver = firebaseApp
+      .auth()
+      .onAuthStateChanged(user => {
+        this.setState({ isSignedIn: !!user });
+      });
     window.addEventListener("resize", this.resizeFunction);
-  };
+  }
   componentDidUpdate(e) {
     if (e.history.location.pathname !== e.location.pathname) {
       this.refs.mainPanel.scrollTop = 0;
@@ -91,59 +92,66 @@ class App extends React.Component {
         this.setState({ mobileOpen: false });
       }
     }
-  };
+  }
   componentWillUnmount() {
     window.removeEventListener("resize", this.resizeFunction);
     this.unregisterAuthObserver();
-  };
+  }
 
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.logo}>
-          <i className={styles.logoIcon + ' material-icons'}>photo</i> My App
+          <i className={styles.logoIcon + " material-icons"}>photo</i> My App
         </div>
         <div className={styles.caption}>This is a cool demo app</div>
-        {this.state.isSignedIn !== undefined && !this.state.isSignedIn &&
-          <div>
-            <StyledFirebaseAuth className={styles.firebaseUi} uiConfig={this.uiConfig}
-                                firebaseAuth={firebaseApp.auth()}/>
-          </div>
-        }
-        {this.state.isSignedIn &&
-          <div className={classes.wrapper}>
-        <Sidebar
-          routes={dashboardRoutes}
-          logoText={"US Entertainment"}
-          logo={logo}
-          image={image}
-          handleDrawerToggle={this.handleDrawerToggle}
-          open={this.state.mobileOpen}
-          color="blue"
-          {...rest}
-        />
-        <div className={classes.mainPanel} ref="mainPanel">
-          <Header
-            routes={dashboardRoutes}
-            handleDrawerToggle={this.handleDrawerToggle}
-            {...rest}
-          />
-          
-          {this.getRoute() ? (
-            <div className={classes.content}>
-              <div className={classes.container}>{switchRoutes}</div>
+        {this.state.isSignedIn !== undefined &&
+          !this.state.isSignedIn && (
+            <div>
+              {/* <StyledFirebaseAuth
+                className={styles.firebaseUi}
+                uiConfig={this.uiConfig}
+                firebaseAuth={firebaseApp.auth()}
+              /> */}
             </div>
-          ) : (
-            <div className={classes.map}>{switchRoutes}</div>
           )}
-          {this.getRoute() ? <Footer /> : null}
-         }
+        {this.state.isSignedIn && (
+          <div className={classes.wrapper}>
+            <Sidebar
+              routes={dashboardRoutes}
+              logoText={"US Entertainment"}
+              logo={logo}
+              image={image}
+              handleDrawerToggle={this.handleDrawerToggle}
+              open={this.state.mobileOpen}
+              color="blue"
+              {...rest}
+            />
+            <div className={classes.mainPanel} ref="mainPanel">
+              <Header
+                routes={dashboardRoutes}
+                handleDrawerToggle={this.handleDrawerToggle}
+                {...rest}
+              />
+
+              {this.getRoute() ? (
+                <div className={classes.content}>
+                  <div className={classes.container}>{switchRoutes}</div>
+                </div>
+              ) : (
+                <div className={classes.map}>{switchRoutes}</div>
+              )}
+              {this.getRoute() ? <Footer /> : null}
+            </div>
+          </div>
+        )}
       </div>
     );
+  }
 }
 
 App.propTypes = {
-  classes : PropTypes.object.isRequired
- };
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(dashboardStyle)(App);
