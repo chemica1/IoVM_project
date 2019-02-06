eb93041f2373250ab536b7702be12f6f2b00ffa2
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-02-07 00:42:42 +0900

    실패

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -18,23 +18,19 @@ import dashboardStyle from "assets/jss/material-dashboard-react/layouts/dashboar
 
 import image from "assets/img/sidebar-2.jpg";
 import logo from "assets/img/reactlogo.png";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-import FirebaseAuth from '../../components/Login/firebaseUI';
-import StyledFirebaseAuth from '../../components/Login/StyledFirebaseUI';
+import firebase from "firebase/app";
+import "firebase/auth";
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-
-import styles from '../../components/Login/app.css'; // This uses CSS modules.
-import '../../components/Login/firebaseui-styling.global.css'; // Import globally.
-import {DB_config} from "config/fbconfig.js"
+import { DB_config } from "config/fbconfig.js";
 
 
-const firebaseApp = firebase.initializeApp(DB_config);
+
 const switchRoutes = (
   <Switch>
     {dashboardRoutes.map((prop, key) => {
-      if (prop.redirect)   //이게 처음 들왔을때 화면이다.
+      if (prop.redirect)
         return <Redirect from={prop.path} to={prop.to} key={key} />;
       return <Route path={prop.path} component={prop.component} key={key} />;
     })}
@@ -42,48 +38,32 @@ const switchRoutes = (
 );
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       mobileOpen: false,
-      isSignedIn: undefined,
+      isSignedIn: false
     };
-    
     this.resizeFunction = this.resizeFunction.bind(this);
   }
-
-  uiConfig = {
-    signInFlow: 'popup',
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false,
-    },
-  };
-
-
   handleDrawerToggle = () => {
     this.setState({ mobileOpen: !this.state.mobileOpen });
   };
   getRoute() {
     return this.props.location.pathname !== "/maps";
-  };
+  }
   resizeFunction() {
     if (window.innerWidth >= 960) {
       this.setState({ mobileOpen: false });
     }
-  };
+  }
   componentDidMount() {
     if (navigator.platform.indexOf("Win") > -1) {
       const ps = new PerfectScrollbar(this.refs.mainPanel);
     }
-    this.unregisterAuthObserver = firebaseApp.auth().onAuthStateChanged((user) => {
-      this.setState({isSignedIn: !!user});
-    });
     window.addEventListener("resize", this.resizeFunction);
-  };
+  }
   componentDidUpdate(e) {
     if (e.history.location.pathname !== e.location.pathname) {
       this.refs.mainPanel.scrollTop = 0;
@@ -91,27 +71,15 @@ class App extends React.Component {
         this.setState({ mobileOpen: false });
       }
     }
-  };
+  }
   componentWillUnmount() {
     window.removeEventListener("resize", this.resizeFunction);
-    this.unregisterAuthObserver();
-  };
-
+  }
   render() {
+    const { classes, ...rest } = this.props;
     return (
-      <div className={styles.container}>
-        <div className={styles.logo}>
-          <i className={styles.logoIcon + ' material-icons'}>photo</i> My App
-        </div>
-        <div className={styles.caption}>This is a cool demo app</div>
-        {this.state.isSignedIn !== undefined && !this.state.isSignedIn &&
-          <div>
-            <StyledFirebaseAuth className={styles.firebaseUi} uiConfig={this.uiConfig}
-                                firebaseAuth={firebaseApp.auth()}/>
-          </div>
-        }
-        {this.state.isSignedIn &&
-          <div className={classes.wrapper}>
+      <div className={classes.wrapper}>
+           
         <Sidebar
           routes={dashboardRoutes}
           logoText={"US Entertainment"}
@@ -128,7 +96,7 @@ class App extends React.Component {
             handleDrawerToggle={this.handleDrawerToggle}
             {...rest}
           />
-          
+          {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
           {this.getRoute() ? (
             <div className={classes.content}>
               <div className={classes.container}>{switchRoutes}</div>
@@ -137,13 +105,14 @@ class App extends React.Component {
             <div className={classes.map}>{switchRoutes}</div>
           )}
           {this.getRoute() ? <Footer /> : null}
-         }
+        </div>
       </div>
     );
+  }
 }
 
 App.propTypes = {
-  classes : PropTypes.object.isRequired
- };
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(dashboardStyle)(App);
